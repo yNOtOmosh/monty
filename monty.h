ccf9df9e638f3baa8d36087f27f73d483cecebf6
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-08-23 12:25:57 -0700

    header file

--- monty.h
+++ monty.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
