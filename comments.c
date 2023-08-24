af8c6bb2812991d5130047e1f8cdfb27df09f751
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-08-24 07:51:40 -0700

    When the first non-space character of a line is #, treat this line as a comment (don’t do anything)

---
+++ comments.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_comment - this handles a line that starts with a
+ * comment character '#'
+ * @head: pointer to the top of the stack
+ * @counter: line number of the command
+ */
+
+void f_comment(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+}
