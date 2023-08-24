24d009818b3af76e05dc759afdc46f18e52f2c2a
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-08-23 22:25:05 -0700

    The opcode nop doesn’t do anything

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop - doesn't do anything
+ * @head: pointer to the top of the stack
+ * @counter: line number of the command
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+}
