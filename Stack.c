8652dd02d53d301b4de0a991d1d9fbd8932dd1cc
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-08-24 09:28:18 -0700

    The opcode stack sets the format of the data to a stack (LIFO). This is the default behavior of the program

---
+++ Stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - sets the format of the data to a stack(LIFO)
+ * @head: pointer to the top of the stack
+ * @counter: line number of the command
+ */
+
+void f_stack(stack_t **head unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
